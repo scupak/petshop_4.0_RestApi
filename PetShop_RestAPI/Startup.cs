e89a65a81ce6693688e2b0118e08ddeb4bf095b7
scupak <anto1095@easv365.dk>
2020-09-07 18:08:35 +0200

    Update Startup.cs

--- PetShop_RestAPI/Startup.cs
+++ PetShop_RestAPI/Startup.cs
@@ -36,8 +36,8 @@ namespace PetShop_RestAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+           // if (env.IsDevelopment())
+           // {
                 app.UseDeveloperExceptionPage();
                 FakeDB.InitData();
                 /**
@@ -48,7 +48,7 @@ namespace PetShop_RestAPI
                     var repo = scope.ServiceProvider.ge
                 }
                 */
-            }
+          //  }
 
             app.UseHttpsRedirection();
 
